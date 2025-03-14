8a6b0657962a2caa7f01f5dc48820b64c9b6f812
Ktoto112 <19.12.2006sasha@gmail.com>
2025-03-14 10:56:32 +0200

    Add files via upload

---
+++ lab8/lab8/lab8.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    const int SIZE = 20;
+    int arr[SIZE];
+    srand(time(0));
+
+
+    for (int i = 0; i < SIZE; i++) {
+        arr[i] = (rand() % 101) - 50;
+    }
+
+
+    cout << "Масив: ";
+    for (int i = 0; i < SIZE; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+
+    int signChanges = 0;
+    for (int i = 1; i < SIZE; i++) {
+        if ((arr[i] > 0 && arr[i - 1] < 0) || (arr[i] < 0 && arr[i - 1] > 0)) {
+            signChanges++;
+        }
+    }
+
+    cout << "Кількість змін знаку: " << signChanges << endl;
+
+    return 0;
+}
